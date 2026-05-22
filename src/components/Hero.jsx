b11177114b0d6fced0ea8b4e37ce6c2a301f54be
supritkolse <samkolse2134@gmail.com>
2026-05-22 15:15:47 +0530

    feat: UI and responsive SpotLight Component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 import { ChevronRight, Download, Globe } from 'lucide-react';
 
